df55e8dafbe10308c421aa88e5b711d3e180ac16
Nick Foley <115027712+Foley-ops@users.noreply.github.com>
2023-01-26 13:20:54 -0600

    Create project

---
+++ DaysTilNextMonth/src/DaysTilNextMonth.java
@@ -0,0 +1,13 @@
+/******************************************************************************
+ Programmer: Nicholas Foley
+ Date: 01/26/2023
+ Lab 4
+ Instructor: Dr. Rafael Azuaje
+ College: Northeast Lakeview College
+
+ *******************************************************************************/
+public class DaysTilNextMonth {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
