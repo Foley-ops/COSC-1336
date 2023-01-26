5edba508033f0f13069a59da5119f1d6522ccb98
Nick Foley <115027712+Foley-ops@users.noreply.github.com>
2023-01-26 13:17:00 -0600

    Complete MathTest lab

--- MathTest/src/MathTest.java
+++ MathTest/src/MathTest.java
@@ -1,5 +1,19 @@
+import static java.lang.Math.*;
+
+/******************************************************************************
+ Programmer: Nicholas Foley
+ Date: 01/26/2023
+ Lab 4
+ Instructor: Dr. Rafael Azuaje
+ College: Northeast Lakeview College
+
+ *******************************************************************************/
 public class MathTest {
     public static void main(String[] args) {
 
+        System.out.println(sqrt(37));
+        System.out.println(sin(300));
+        System.out.println(cos(300));
+        System.out.println((int)(20 * random()));
     }
 }
