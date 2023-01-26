2af5a61155b75f24befbdd582aa420a98fef26f5
Nick Foley <115027712+Foley-ops@users.noreply.github.com>
2023-01-26 15:56:22 -0600

    Finish ten thousand days project

--- DaysTilNextMonth/src/DaysTilNextMonth.java
+++ DaysTilNextMonth/src/DaysTilNextMonth.java
@@ -12,18 +12,20 @@ import java.util.Scanner;
 public class DaysTilNextMonth {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        LocalDate orderDate;
 
-        final int WEEKS_FOR_DELIVERY = 2;
-        System.out.print("Enter order month ");
+        final int tenThousand = 10_000;
+        System.out.print("Enter birth month (MM): ");
         int mo = input.nextInt();
-        System.out.print("Enter order day ");
+        System.out.print("Enter birth day (DD): ");
         int day = input.nextInt();
-        System.out.print("Enter order year ");
+        System.out.print("Enter birth year (YYYY): ");
         int year = input.nextInt();
-        orderDate = LocalDate.of(year, mo, day);
-        System.out.println("Order date is " + orderDate);
-        System.out.println("Delivery date is " +
-                orderDate.plusWeeks(WEEKS_FOR_DELIVERY));
+
+        LocalDate birthDay = LocalDate.of(year, mo, day);
+
+        LocalDate dateAfterTenThousandDays = birthDay.plusDays(tenThousand);
+
+        System.out.println("You were born on " + birthDay);
+        System.out.println("You turned ten thousand days old on " + dateAfterTenThousandDays);
     }
 }
