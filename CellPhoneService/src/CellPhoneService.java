c6b3b1adeb5a2315511b411d0c44edcda51dfaeb
Nick <115027712+Foley-ops@users.noreply.github.com>
2023-02-01 12:20:54 -0600

    change recommendedPlan method

--- CellPhoneService/src/CellPhoneService.java
+++ CellPhoneService/src/CellPhoneService.java
@@ -20,36 +20,32 @@ public class CellPhoneService {
         System.out.print("Data used (Gigabytes): ");
         double dataUsage = scanner.nextDouble();
 
-        String response = (recommendedPlan(talkMinutes, textMessages, dataUsage));
+        String recommendedPlan = (recommendedPlan(talkMinutes, textMessages, dataUsage));
 
-        System.out.println("We recommend " + response + " a month.");
+        System.out.println("We recommend " + recommendedPlan + " a month.");
     }
 
     public static String recommendedPlan(double talkMinutes, int textMessages, double dataUsage) {
 
-        String purpose;
-
         if (talkMinutes < 500 && textMessages == 0 && dataUsage == 0)
-            purpose = ("Plan A for $49");
+            return("Plan A for $49");
 
         else if (talkMinutes < 500 &&  0 <= textMessages && dataUsage == 0)
-            purpose = ("Plan B for $55");
+            return("Plan B for $55");
 
         else if (500 <= talkMinutes && 0 <= textMessages && textMessages < 100 && dataUsage == 0)
-            purpose = ("Plan C for $61");
+            return("Plan C for $61");
 
         else if (talkMinutes >= 500 && textMessages >= 100 && dataUsage == 0)
-            purpose = ("Plan D for $70");
+            return("Plan D for $70");
 
         else if (0 < dataUsage && dataUsage < 2 )
-            purpose = ("Plan E for $79");
+            return("Plan E for $79");
 
         else if (2 < dataUsage )
-            purpose = ("Plan F for $87");
+            return("Plan F for $87");
 
         else
-            purpose = ("an extended service plan for an undefined amount");
-
-        return purpose;
+            return("an extended service plan for an undefined amount");
     }
 }
