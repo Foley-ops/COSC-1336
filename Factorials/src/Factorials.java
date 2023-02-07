66d1f2314a9ab42a5da7772cfc11bf7ab027ea2e
Nick <115027712+Foley-ops@users.noreply.github.com>
2023-02-07 13:22:35 -0600

    Update Factorials.java

--- Factorials/src/Factorials.java
+++ Factorials/src/Factorials.java
@@ -7,7 +7,7 @@
  *******************************************************************************/
 public class Factorials {
     public static void main(String[] args) {
-        for (int i = 1; i <= 20; i++) {
+        for (int i = 1; i <= 10; i++) {
             long factorial = 1L;
             for (int j = 1; j <= i; j++) {
                 factorial *= j;
