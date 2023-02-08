8269e1c2e0f97217276c695bd101eb3a86075104
Nick Foley <115027712+Foley-ops@users.noreply.github.com>
2023-02-08 15:15:31 -0600

    Add arguments

--- ThreeLetterAcronym/src/ThreeLetterAcronym.java
+++ ThreeLetterAcronym/src/ThreeLetterAcronym.java
@@ -1,6 +1,13 @@
+import java.util.Scanner;
+
 public class ThreeLetterAcronym {
     public static void main(String[] args) {
-        String FBI = "FBI";
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Please enter three words to turn into an acronym: ");
 
+        String userInput = scanner.nextLine();
+        System.out.println(userInput);
+
+        scanner.close();
     }
 }
