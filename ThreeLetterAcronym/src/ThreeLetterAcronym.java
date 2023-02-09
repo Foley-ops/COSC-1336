94295d5cb8d806d027e618672e479ad96c12136d
Nick <115027712+Foley-ops@users.noreply.github.com>
2023-02-08 20:15:47 -0600

    Working beta

--- ThreeLetterAcronym/src/ThreeLetterAcronym.java
+++ ThreeLetterAcronym/src/ThreeLetterAcronym.java
@@ -1,13 +1,28 @@
 import java.util.Scanner;
-
 public class ThreeLetterAcronym {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Please enter three words to turn into an acronym: ");
 
         String userInput = scanner.nextLine();
-        System.out.println(userInput);
+        System.out.println(getAcronym(userInput));
 
         scanner.close();
+    }
+    public static String getAcronym(String userInput) {
+        String fullAcronym = "";
+        boolean spaceFinder = true;
+
+        for ( int i = 0; i < userInput.length(); i++ ) {
+            if (userInput.charAt(i) == ' ')
+                spaceFinder = true;
+
+            else if (userInput.charAt(i) != ' ' && spaceFinder == true) {
+                fullAcronym += (userInput.charAt(i));
+                spaceFinder = false;
+            }
+        }
+        return fullAcronym.toUpperCase();
+
     }
 }
