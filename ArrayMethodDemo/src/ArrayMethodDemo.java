a2bb68e26d655473fc8bcc90cbb22d3b57cee38c
Nick <115027712+Foley-ops@users.noreply.github.com>
2023-02-10 15:47:33 -0600

    Project completed

--- ArrayMethodDemo/src/ArrayMethodDemo.java
+++ ArrayMethodDemo/src/ArrayMethodDemo.java
@@ -1,6 +1,14 @@
+/******************************************************************************
+ Programmer: Nicholas Foley
+ Date: 02/10/2023
+ Lab 8
+ Instructor: Dr. Rafael Azuaje
+ College: Northeast Lakeview
+ *******************************************************************************/
 import java.util.Random;
 public class ArrayMethodDemo {
     public static void main(String[] args) {
+        // Creates array with ten random integers
         int[] array = new int[10];
         Random random = new Random();
 
@@ -8,10 +16,9 @@ public class ArrayMethodDemo {
             array[i] = random.nextInt(100);
         }
 
+        // Displays all parameters in prompt
         System.out.println("Numbers in array: ");
-        for (int number : array) {
-            System.out.print(number + " ");
-        }
+        numbersInArray(array);
 
         System.out.println("\nNumbers reversed: ");
         reversed(array);
@@ -25,12 +32,20 @@ public class ArrayMethodDemo {
         System.out.println("\nNumbers above average: ");
         higherThanAverage(array, sum(array));
     }
+    private static void numbersInArray(int[] array) {
+        // Prints all numbers in array
+        for (int number : array) {
+            System.out.print(number + " ");
+        }
+    }
     private static void reversed(int[] array) {
+        // Reverses order of integers in array
         for (int i = array.length - 1; i >= 0; i--) {
             System.out.print(array[i] + " ");
         }
     }
     private static int sum(int[] array) {
+        // Calculates sum of array
         int sum = 0;
         for (int number : array) {
             sum += number;
@@ -38,6 +53,7 @@ public class ArrayMethodDemo {
         return sum;
     }
     private static void lessThanFifty(int[] array) {
+        // Prints integers in array under fifty
         for (int number : array) {
             if (number < 50) {
                 System.out.print(number + " ");
@@ -45,7 +61,9 @@ public class ArrayMethodDemo {
         }
     }
     private static void higherThanAverage(int[] array, int sum) {
+        // Calculates average
         int average = sum / array.length;
+        // Iterates through array looking for values above the average
         for (int number : array) {
             if (number > average) {
                 System.out.print(number + " ");
