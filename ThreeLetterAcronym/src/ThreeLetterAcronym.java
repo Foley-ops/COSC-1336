da385290bbb59e6ade54a9422108dde00c1574af
Nick <115027712+Foley-ops@users.noreply.github.com>
2023-02-10 09:59:28 -0600

    Finished project

--- ThreeLetterAcronym/src/ThreeLetterAcronym.java
+++ ThreeLetterAcronym/src/ThreeLetterAcronym.java
@@ -1,32 +1,39 @@
+
+/******************************************************************************
+ Programmer: Nicholas Foley
+ Date: 02/10/2023
+ Lab 7
+ Instructor: Dr. Rafael Azuaje
+ College: Northeast Lakeview
+ *******************************************************************************/
 import java.util.Scanner;
 public class ThreeLetterAcronym {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Please enter three words to turn into an acronym: ");
 
-        String userInput = scanner.nextLine().toUpperCase();
-
-        System.out.println(getAcronym(userInput));
+        String userInput = scanner.nextLine();
+        // Calls method and capitalizes result
+        System.out.println("The acronym is: " + getAcronym(userInput).toUpperCase());
 
         scanner.close();
-
     }
 
     public static String getAcronym(String userInput) {
-        String returnStatement = "";
-        int wordCount = 1;
-        returnStatement += userInput.charAt(0);
+        // Error handling for extra spaces
+        String[] words = userInput.split("\\s+");
 
-        for (int i = 0; i < userInput.length(); i++) {
-            if (wordCount < 4 && userInput.charAt(i) == ' ') {
-                wordCount++;
+        // Method return value created
+        String acronym = "";
 
-            } else if (wordCount < 4 && userInput.charAt(i) != ' ') {
-                returnStatement += userInput.charAt(i);
-            }
-            else
-                System.out.println("ERROR");
+        /*
+        Loop through array for first 3 words
+        add error handling if user inputs more than three words
+        */
+        for (int i = 0; i < 3 && i < words.length; i++) {
+            acronym += words[i].charAt(0);
         }
-        return returnStatement;
+
+        return acronym;
     }
 }
