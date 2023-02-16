ddefb49f6205f9f7763a14533c69ae1f70ccce9a
Nick Foley <115027712+Foley-ops@users.noreply.github.com>
2023-02-16 14:48:37 -0600

    Add lab 10

---
+++ Horse/src/Horse.java
@@ -0,0 +1,17 @@
+/*Create a class named Horse that contains data fields for the name, color, and birth year.
+Include get and set methods for these fields. Next, create a subclass named RaceHorse, which contains
+an additional field that holds the number of races in which the horse has competed and additional
+methods to get and set the new field. Write an application that demonstrates using objects of each
+class. Save the files as Horse.java, RaceHorse.java, and DemoHorses.java*/
+/******************************************************************************
+ Programmer: Nicholas Foley
+ Date: 02/16/2023
+ Lab 10
+ Instructor: Dr. Rafael Azuaje
+ College: Northeast Lakeview College
+ *******************************************************************************/
+public class Horse {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ Horse/src/RaceHorse.java
@@ -0,0 +1,9 @@
+/******************************************************************************
+ Programmer: Nicholas Foley
+ Date: 02/16/2023
+ Lab 10
+ Instructor: Dr. Rafael Azuaje
+ College: Northeast Lakeview College
+ *******************************************************************************/
+public class RaceHorse {
+}
