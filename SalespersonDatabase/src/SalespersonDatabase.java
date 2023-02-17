db6ea5a729d60932fd7a2b0ca42a0e95dba4e101
Nick Foley <115027712+Foley-ops@users.noreply.github.com>
2023-02-17 14:00:37 -0600

    Not working, need break

--- SalespersonDatabase/src/SalespersonDatabase.java
+++ SalespersonDatabase/src/SalespersonDatabase.java
@@ -19,6 +19,8 @@ After each option executes, display the updated database in ascending order by S
 and prompt the user to select the next action. Save the application as SalespersonDatabase.java.
 */
 
+import java.util.Scanner;
+
 /******************************************************************************
  Programmer: Nicholas Foley
  Date: 02/16/2023
@@ -28,6 +30,61 @@ and prompt the user to select the next action. Save the application as Salespers
  *******************************************************************************/
 public class SalespersonDatabase {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        Boolean addData = true;
+
+        System.out.print("How many salespeople would you like to enter? (max 20): ");
+        int amountSalespeople = scanner.nextInt();
+
+
+
+        while (addData) {
+
+            Salesperson[] salespersonsArray = new Salesperson[amountSalespeople];
+            System.out.print("Would you like to ADD, DELETE, or CHANGE a record?  Type YES or NO.");
+            String answer = scanner.nextLine().toUpperCase();
+
+            if (answer.equals("NO"))
+                addData = false;
+            else {
+                System.out.print("Would you like to ADD, DELETE, or CHANGE a record?");
+                String answer2 = scanner.nextLine().toUpperCase();
+
+                if (answer2.equals("ADD") && salespersonsArray.length >= 20) {
+                    System.out.println("Error: database is full");
+                    addData = false;
+                } else if (answer2.equals("ADD")) {
+                    System.out.print("Please enter an ID number: ");
+                    int salesIDNumber = scanner.nextInt();
+                    for (int i = 0; i < salespersonsArray.length; i++) {
+                        if (i == salespersonsArray[i].getId()) {
+                            System.out.println("Error: Sales ID already exists.");
+                        } else {
+                            System.out.print("Please enter sales value: ");
+                            double salesValue = scanner.nextDouble();
+                            new Salesperson(salesIDNumber, salesValue);
+                        }
+                    }
+
+                }
+                if (answer2.equals("DELETE") && salespersonsArray.length == 0) {
+                    System.out.println("Error: Database is empty");
+                    addData = false;
+                } else {
+                    System.out.print("Please enter Sales ID to delete: ");
+                    int salesIDNumber = scanner.nextInt();
+                    for (int i = 0; i < salespersonsArray.length; i++) {
+                        if (i != salespersonsArray[i].getId()) {
+                            System.out.println("Error: Sales ID does not exists.");
+                            addData = false;
+                        }
+                    }
+                }
+                if (answer2.equals("CHANGE") && salespersonsArray.length == 0) {
+                    System.out.println("Error: Database is empty");
+                    addData = false;
+                }
+            }
+        }
     }
 }
