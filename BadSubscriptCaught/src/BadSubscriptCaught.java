76710324944df205ad6efdf1a1b2f63aab8cada0
Nick <115027712+Foley-ops@users.noreply.github.com>
2023-02-20 15:30:09 -0600

    Working beta

--- BadSubscriptCaught/src/BadSubscriptCaught.java
+++ BadSubscriptCaught/src/BadSubscriptCaught.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 /*Write an application named BadSubscriptCaught in which you declare an array of 10 first names.
 Write a try block in which you prompt the user for an integer and display the name in the requested position.
 Create a catch block that catches the potential ArrayIndexOutOfBoundsException thrown when the user
@@ -13,8 +15,19 @@ enters a number that is out of range. The catch block should also display an err
 public class BadSubscriptCaught {
     public static void main(String[] args) {
 
-        String[] array = { "Albert", "Billy", "Charles", "Daniel", "Elizabeth",
+        // Declaring array of 10 first names 
+        String[] firstNames = { "Albert", "Billy", "Charles", "Daniel", "Elizabeth",
                 "Francine", "George", "Henry", "Igor", "Jacen"};
-    }
 
+        // Try block to prompt user for an integer and display name at requested position
+        try {
+             System.out. print("Enter an integer between 1 and 10: ");
+             Scanner scanner = new Scanner(System.in); 
+             int number = (scanner.nextInt()) - 1; 
+             
+             System.out.println("Name at position " + (number + 1) + " is "+ firstNames[number]);
+        } catch (ArrayIndexOutOfBoundsException e) {
+            System.err.println("Error! Number entered is out of range!"); 
+        }
+    }
 }
