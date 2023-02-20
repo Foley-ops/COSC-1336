4f2aa59ff95c12199007e872126718f0c62ae9d2
Nick <115027712+Foley-ops@users.noreply.github.com>
2023-02-20 14:45:24 -0600

    Creat Lab 11 and 12

---
+++ BadSubscriptCaught/src/BadSubscriptCaught.java
@@ -0,0 +1,17 @@
+/*Write an application named BadSubscriptCaught in which you declare an array of 10 first names.
+Write a try block in which you prompt the user for an integer and display the name in the requested position.
+Create a catch block that catches the potential ArrayIndexOutOfBoundsException thrown when the user
+enters a number that is out of range. The catch block should also display an error message.
+ */
+/******************************************************************************
+ Programmer: Nicholas Foley
+ Date: 02/20/2023
+ Lab 12
+ Instructor: Dr. Rafael Azuaje
+ College: Northeast Lakeview
+ *******************************************************************************/
+public class BadSubscriptCaught {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ BookArray/src/BookArray.java
@@ -0,0 +1,16 @@
+/*
+Write an application named BookArray in which you create an array that holds 10 Books, some Fiction and some NonFiction.
+Using a for loop, display details about all 10 books. Save the file as BookArray.java.
+ */
+/******************************************************************************
+ Programmer: Nicholas Foley
+ Date: 02/20/2023
+ Lab 11
+ Instructor: Dr. Rafael Azuaje
+ College: Northeast Lakeview
+ *******************************************************************************/
+public class BookArray {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
