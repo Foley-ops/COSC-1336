a20d3ccc3d07d74250dae27880eb81b347cf9089
Nick Foley <115027712+Foley-ops@users.noreply.github.com>
2023-02-28 05:48:03 -0600

    Add lab 13

---
+++ ValidateCheckDigits/src/ValidateCheckDigits.java
@@ -0,0 +1,17 @@
+/*Using a text editor, create a file that contains a list of at least 10 six-digit account numbers.
+Read in each account number and display whether it is valid. An account number is valid only if the
+last digit is equal to the sum of the first five digits divided by 10. For example, the number 223355
+ is valid because the sum of the first five digits is 15, the remainder when 15 is divided by 10 is 5,
+ and the last digit is 5. Write only valid account numbers to an output file, each on its own line. */
+/******************************************************************************
+ Programmer: Nicholas Foley
+ Date: 02/28/2023
+ Lab 13
+ Instructor: Dr. Rafael Azuaje
+ College: Northeast Lakeview
+ *******************************************************************************/
+public class ValidateCheckDigits {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
